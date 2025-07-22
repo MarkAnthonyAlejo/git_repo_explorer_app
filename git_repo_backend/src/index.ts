a0b2db9de72cc0b03b897a5d52782a0573160b58
Anthony Alejo <164105917+MarkAnthonyAlejo@users.noreply.github.com>
2025-07-22 15:13:34 -0700

    Updated index.js removed localhost

--- git_repo_backend/src/index.ts
+++ git_repo_backend/src/index.ts
@@ -7,7 +7,6 @@ dotenv.config();
 
 const app = express();
 const allowedOrigins = [
-   'http://localhost:3000',
   'https://git-repo-explorer-app.vercel.app', 
   'https://git-repo-explorer-app-1.onrender.com'
 ];
